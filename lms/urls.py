89cc9370949bc4d855f808469f19000c3a81e565
Mihouk <arbi_14@mail.ru>
2025-02-15 18:07:33 +0300

    HW_32.1 validators.py

--- lms/urls.py
+++ lms/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from lms.views import (CourseViewSet, LessonCreateApiView,
-                       LessonListApiView, LessonRetrieveApiView, LessonUpdateApiView, LessonDestroyApiView)
+                       LessonListApiView, LessonRetrieveApiView, LessonUpdateApiView, LessonDestroyApiView,
+                       SubscriptionApiView)
 from lms.apps import LmsConfig
 
 app_name = LmsConfig.name
 router = SimpleRouter()
 router.register("", CourseViewSet)
-
 
 urlpatterns = [
     path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
@@ -15,7 +15,7 @@ urlpatterns = [
     path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
     path("lessons/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lessons_delete"),
     path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
+    path("subscribe/", SubscriptionApiView.as_view(), name="subscription"),
 ]
-
 
 urlpatterns += router.urls
